187ae2f1969192ac5a14840b77c1cd30590df817
Muhammad Kashif Shamim <muhammadkashif92@hotmail.com>
2022-10-31 11:08:08 +0500

    Integrated Next Auth with Github

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -5,6 +5,7 @@ import ReactMarkdown from "react-markdown"
 import Layout from "../../components/Layout"
 import { PostProps } from "../../components/Post"
 import prisma from '../../lib/prisma';
+
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     const post = await prisma.article.findUnique({
       where: {
